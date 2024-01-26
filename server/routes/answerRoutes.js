81034b9f997841c83cada7757b96a84dd5577d73
Bini-tech27 <124435014+Bini-tech27@users.noreply.github.com>
2024-01-26 09:53:58 +0300

    routes updated

--- server/routes/answerRoutes.js
+++ server/routes/answerRoutes.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const auth = require("../middleware/auth");
-const {
-  allAnswers,
-  postAnswers,
-} = require("../controller/answerController");
+const { getAnswers, postAnswers } = require("../controller/answerController");
 const router = express.Router();
 
 //post answer route
-router.post("/post-answers", auth, postAnswers);
+router.post("/post-answers/:questionId", auth,  postAnswers);
 
 //all answer route
-router.get("/answers",auth, allAnswers);
+router.get("/answers/:questionId", auth, getAnswers);
 
 module.exports = router;

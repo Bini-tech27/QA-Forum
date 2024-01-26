3a714236374a33fcc675a8f31e28e1e4d9dd158a
Bini-tech27 <124435014+Bini-tech27@users.noreply.github.com>
2024-01-26 07:46:14 +0300

    middleware updated

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,31 +4,28 @@ const prisma = new PrismaClient();
 
 const auth = async (req, res, next) => {
   const token = req.headers.authorization;
-//   const splited = token.split(" ");
-//   console.log(splited[1], "dfghjkfghj");
+  const splitted = token.split(" ")[1];
 
-  if (!token) {
+  if (!splitted) {
     return res.status(401).json({ msg: "unauthorized user" });
   }
 
   try {
-    const decoded = jwt.verify(token, "secret-key");
-    console.log(decoded, "dec");
+    const decoded = jwt.verify(splitted, "secret-key");
     const user = await prisma.user.findUnique({
       where: {
-        userId: decoded.id,
+        userId: decoded.userId,
       },
     });
-
     if (!user) {
-      return res.status(401).json({ msg: "Unauthorized - Invalid user" });
+      return res.status(401).json({ msg: "Unauthorized Invalid users" });
     }
 
-    req.user = { user };
+    req.user = user;
     next();
   } catch (error) {
     console.error(error.message);
-    return res.status(401).json({ msg: "Unauthorized - Invalid users" });
+    return res.status(401).json({ msg: "Authentication-Invalid" });
   }
 };
 
